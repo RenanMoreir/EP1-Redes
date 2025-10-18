8a1a2a33e2eda691aa4a1e906bea934e99ef2f02
Renan <renan_moreira@usp.br>
2025-10-18 11:36:42 -0300

    primeiros passos

---
+++ src/Client.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client implements Runnable{
+    private static final  String SERVER_ADRESS = "127.0.0.1";
+   private  static final int serverSocket = 4000;
+    private ClientSocket clientSocket;
+
+    private Scanner scanner;
+
+    public Client(){
+        scanner = new Scanner(System.in);
+    }
+
+    public void start(){
+        try {
+            System.out.println("Cliente conectado ao servidor em "  + SERVER_ADRESS + " Porta: " + serverSocket);
+            clientSocket = new ClientSocket(new Socket(SERVER_ADRESS, serverSocket));
+            new Thread(this).start();
+            messageLoop();
+        }
+        catch (IOException ex){
+            System.out.println("Erros ao iniciar o Cliente " + ex.getMessage());
+        }
+        finally {
+            clientSocket.close();
+        }
+    }
+
+    @Override
+    public void run(){
+        String msg;
+        while ((msg = clientSocket.getMessage()) != null )
+            System.out.println("Msg recebida:" + msg);
+    }
+
+    private void  messageLoop(){
+        try{
+            String msg;
+            do{
+                System.out.print("Digite uma mensagem: ");
+                msg = scanner.nextLine();
+                clientSocket.sendMsg(msg);
+            } while (!msg.equalsIgnoreCase("sair"));
+        } catch (Exception e) {
+            System.out.print("Erros ao enviar a mensagem " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Server {
+    private static final  int PORT = 4000;
+    private ServerSocket serverSocket;
+    private final List<ClientSocket> clientes = new LinkedList<>();
+
+    public void start(){
+        try{
+            System.out.println("Servidor Iniciado");
+            serverSocket = new ServerSocket(PORT);
+            clientConnectionLoop();
+        }
+        catch (IOException ex){
+            System.out.println("Erros ao iniciar o servidor " + ex.getMessage());
+        }
+    }
+
+    private void clientConnectionLoop(){
+        try {
+            while (true){
+                ClientSocket clientSocket = new ClientSocket(serverSocket.accept());
+                clientes.add(clientSocket);
+                new Thread(() -> clientMessageLoop(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao conectar cliente " + e.getMessage());
+        }
+    }
+
+    private void sendMsgToALL(ClientSocket sender, String msg){
+        Iterator<ClientSocket> iterator = clientes.iterator();
+        for(ClientSocket clientSocket: clientes){
+            if(!sender.equals(clientSocket))
+                clientSocket.sendMsg(msg);
+        }
+    }
+
+    private void clientMessageLoop(ClientSocket clientSocket){
+        String msg;
+        try {
+            while ((msg = clientSocket.getMessage()) != null){
+                if(!"sair".equalsIgnoreCase(msg)){
+                    System.out.println("Msg recebida do cliente " + clientSocket.getRemoteSocketAddress() + ": " + msg);
+                    sendMsgToALL(clientSocket,msg);
+                }else {
+                    return;
+                }
+            }
+        }
+        finally {
+            clientSocket.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+}
